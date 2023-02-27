808441d464e6c91a06e3b027784346f174b0c420
Geunhee Park <118827067+Pakgeunhee@users.noreply.github.com>
2023-02-27 17:34:13 +0900

    [level 0] Title: 나이 출력, Time: 0.01 ms, Memory: 88 MB -BaekjoonHub

--- 프로그래머스/lv0/120820. 나이 출력/나이 출력.java
+++ 프로그래머스/lv0/120820. 나이 출력/나이 출력.java
@@ -1,7 +1,10 @@
 class Solution {
     public int solution(int age) {
         int answer = 0;
-        answer = 2022 - age + 1;
+        int year = 2022;
+        if(0<age&&age<=120) {
+            answer = year - age + 1;
+        }        
         return answer;
     }
 }
