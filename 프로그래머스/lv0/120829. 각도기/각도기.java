7843b75c63a1cb53c6843e7efd368523f765e649
Geunhee Park <118827067+Pakgeunhee@users.noreply.github.com>
2023-07-27 14:19:51 +0900

    [level 0] Title: 각도기, Time: 0.02 ms, Memory: 75.1 MB -BaekjoonHub

--- 프로그래머스/lv0/120829. 각도기/각도기.java
+++ 프로그래머스/lv0/120829. 각도기/각도기.java
@@ -1,15 +1,10 @@
 class Solution {
     public int solution(int angle) {
+        boolean val = 0<angle&&angle<=180;
         int answer = 0;
-        if(0<angle&&angle<90) {
-            answer = 1;
-        } else if(angle == 90) {
-            answer = 2;
-        } else if(90<angle&&angle<180) { 
-            answer = 3;
-        } else if(angle == 180) {
-            answer = 4;
-        }
+        if(val) {
+            answer = (angle == 180) ? 4 : (angle > 90) ? 3 : (angle == 90) ? 2 : 1;
+        }        
         return answer;
     }
 }
