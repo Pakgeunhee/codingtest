6cb57df10dd6c723da5c05ec2bdd262514f36a97
Geunhee Park <118827067+Pakgeunhee@users.noreply.github.com>
2023-07-27 14:07:52 +0900

    [level 0] Title: 두 수의 나눗셈, Time: 0.02 ms, Memory: 73 MB -BaekjoonHub

--- 프로그래머스/lv0/120806. 두 수의 나눗셈/두 수의 나눗셈.java
+++ 프로그래머스/lv0/120806. 두 수의 나눗셈/두 수의 나눗셈.java
@@ -1,11 +1,11 @@
 class Solution {
     public int solution(int num1, int num2) {
+        boolean val = (0<num1&&num1<=100) && (0<num2&&num2<=100);
         int answer = 0;
-        
-        double result = ((double)num1 / num2) * 1000;
-        
-        answer = (int)result;
-
+        if(val) {           
+            double num3 = (double)num1 / num2;
+            answer = (int)(num3 * 1000);
+        }
         return answer;
     }
 }
