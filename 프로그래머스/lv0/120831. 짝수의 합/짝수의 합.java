c59297336850a69beba64e84e07fd44dde9c689b
Geunhee Park <118827067+Pakgeunhee@users.noreply.github.com>
2023-07-27 14:25:34 +0900

    [level 0] Title: 짝수의 합, Time: 0.02 ms, Memory: 68.2 MB -BaekjoonHub

--- 프로그래머스/lv0/120831. 짝수의 합/짝수의 합.java
+++ 프로그래머스/lv0/120831. 짝수의 합/짝수의 합.java
@@ -1,9 +1,10 @@
 class Solution {
     public int solution(int n) {
         int answer = 0;
-        for(int i = 0; i <= n; i++) {
-            if(i % 2 == 0) {
-                answer += i;
+        if(0<n&&n<=1000) {
+            for(int i = 0; i <= n; i++) {
+                int val = (i % 2) == 0 ? i : 0;
+                answer += val;
             }
         }
         return answer;
