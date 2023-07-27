c57e85d3dc22c4886b2f0b0125078158ef6d023d
Geunhee Park <118827067+Pakgeunhee@users.noreply.github.com>
2023-07-27 18:05:05 +0900

    [level 0] Title: 짝수 홀수 개수, Time: 0.01 ms, Memory: 78.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120824. 짝수 홀수 개수/짝수 홀수 개수.java
+++ 프로그래머스/lv0/120824. 짝수 홀수 개수/짝수 홀수 개수.java
@@ -1,8 +1,7 @@
 class Solution {
     public int[] solution(int[] num_list) {
-        int[] answer = {};
-        answer = new int[2];
-        for(int i = 0; i < num_list.length; i++) { 
+        int[] answer = {0,0};
+        for(int i = 0; i < num_list.length; i++) {
             if(num_list[i] % 2 == 0) {
                 answer[0]++;
             } else {
