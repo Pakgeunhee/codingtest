e1c46806785346bc8217d7a03ce539fe1e8c7ae8
Geunhee Park <118827067+Pakgeunhee@users.noreply.github.com>
2023-07-27 18:08:33 +0900

    [level 0] Title: 아이스 아메리카노, Time: 0.02 ms, Memory: 76.6 MB -BaekjoonHub

--- 프로그래머스/lv0/120819. 아이스 아메리카노/아이스 아메리카노.java
+++ 프로그래머스/lv0/120819. 아이스 아메리카노/아이스 아메리카노.java
@@ -1,9 +1,9 @@
 class Solution {
     public int[] solution(int money) {
-        int[] answer = {};
-        answer = new int[2];
-        answer[0] = money / 5500;
-        answer[1] = money % 5500;
+        int[] answer = new int[2];
+        int coffee = money / 5500;
+        answer[0] = coffee;
+        answer[1] = money - (coffee * 5500);
         return answer;
     }
 }
