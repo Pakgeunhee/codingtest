25f326277a6bce41c0a72248b374472db3505c5c
Geunhee Park <118827067+Pakgeunhee@users.noreply.github.com>
2023-07-27 15:18:11 +0900

    [level 0] Title: 피자 나눠 먹기 (1), Time: 0.03 ms, Memory: 72.6 MB -BaekjoonHub

--- 프로그래머스/lv0/120814. 피자 나눠 먹기 （1）/피자 나눠 먹기 （1）.java
+++ 프로그래머스/lv0/120814. 피자 나눠 먹기 （1）/피자 나눠 먹기 （1）.java
@@ -1,7 +1,9 @@
 class Solution {
     public int solution(int n) {
-        int answer = 0; 
-        answer = (n % 7) == 0 ? n/7 : n/7 + 1; 
+        int answer = 0;
+        if(1<=n&&n<=100) {
+            answer = (n % 7 == 0) ? n / 7 : (n/7) + 1; 
+        }
         return answer;
     }
 }
