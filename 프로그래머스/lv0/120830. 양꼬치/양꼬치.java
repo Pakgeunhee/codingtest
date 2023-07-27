2fc6b3b472b8919b2a1ddfb6a56d30d0f568c2d8
Geunhee Park <118827067+Pakgeunhee@users.noreply.github.com>
2023-07-27 14:57:13 +0900

    [level 0] Title: 양꼬치, Time: 0.02 ms, Memory: 72.6 MB -BaekjoonHub

--- 프로그래머스/lv0/120830. 양꼬치/양꼬치.java
+++ 프로그래머스/lv0/120830. 양꼬치/양꼬치.java
@@ -1,7 +1,11 @@
 class Solution {
     public int solution(int n, int k) {
+        boolean val = (0<n&&n<1000) && ((n/10)<=k&&k<1000);
         int answer = 0;
-        answer = (n * 12000) + (k * 2000) - (n/10 * 2000);
+        int service = n / 10;
+        if(val) {
+            answer = (n * 12000) + ((k-service) * 2000);
+        }
         return answer;
     }
 }
