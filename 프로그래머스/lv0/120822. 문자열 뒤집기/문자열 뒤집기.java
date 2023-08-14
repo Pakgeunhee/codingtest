11b43c4fdb8cf49d252333bd91322368ade7b1b9
Geunhee Park <118827067+Pakgeunhee@users.noreply.github.com>
2023-08-14 15:19:36 +0900

    [level 0] Title: 문자열 뒤집기, Time: 31.49 ms, Memory: 85.3 MB -BaekjoonHub

--- 프로그래머스/lv0/120822. 문자열 뒤집기/문자열 뒤집기.java
+++ 프로그래머스/lv0/120822. 문자열 뒤집기/문자열 뒤집기.java
@@ -1,8 +1,9 @@
 class Solution {
     public String solution(String my_string) {
         String answer = "";
-        StringBuilder sb = new StringBuilder(my_string);
-        answer = sb.reverse().toString();
+        for(int i = my_string.length() -1; i >= 0; i--) {
+            answer += my_string.charAt(i);
+        }
         return answer;
     }
 }
